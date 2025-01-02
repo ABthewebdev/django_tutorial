4a334661e41eecbb3171aca10b8c1e111e35ca74
alexml <alexgbrewster@gmail.com>
2025-01-02 00:50:01 -0500

    first commit

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import ToDoList
 # Create your views here.
+
+def index(response, id):
+    ls = ToDoList.objects.get(id = id)
+    item = ls.item_set.get(id=1)
+    return render(response, "main/base.html", {"name": ls.name})
+
+def home(response):
+    return render(response, "main/home.html", {"name": "test"})
