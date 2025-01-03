477dbce8782948943f7db304d0df6fcfd769040d
alexml <alexgbrewster@gmail.com>
2025-01-03 14:50:25 -0500

    problem solved

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("create/", views.create, name='create'),
-    path("<int:id>/", views.index, name='index'),
+    path("<str:name>/", views.index, name='index'),
     path('', views.home, name='home')
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -5,7 +5,7 @@ from .forms import CreateNewList
 
 def index(response, name):
     ls = ToDoList.objects.get(name = name)
-    return render(response, "main/list.html", {"name": ls})
+    return render(response, "main/list.html", {"ls": ls})
 
 def home(response):
     return render(response, "main/home.html", {"name": "test"})
