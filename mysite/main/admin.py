63282124bb576b7b824e28a11d1deb468d7691ab
alexml <alexgbrewster@gmail.com>
2025-01-03 01:12:12 -0500

    second commit

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from .models import ToDoList
+from .models import ToDoList, Item
+from django.contrib.auth import authenticate
 # Register your models here.
 admin.site.register(ToDoList)
+admin.site.register(Item)
+
+username = "alexml"
+password = "Trtploergvui1"
+
+user = authenticate(username=username, password=password)
+if user is not None and user.is_staff:
+    print("Logged in as:", user)
+else:
+    print("Authentication failed.")
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from .models import ToDoList
+from .forms import CreateNewList
 # Create your views here.
 
-def index(response, id):
-    ls = ToDoList.objects.get(id = id)
-    item = ls.item_set.get(id=1)
-    return render(response, "main/base.html", {"name": ls.name})
+def index(response, name):
+    ls = ToDoList.objects.get(name = name)
+    return render(response, "main/list.html", {"ls": ls})
 
 def home(response):
     return render(response, "main/home.html", {"name": "test"})
+
+def create(response):
+    # if (response.method == "POST"):
+    form = CreateNewList(response.POST)
+    return render(response, "main/create.html", {"form": form})
